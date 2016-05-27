92f57dc14c9a94181eeb5a68cabba1ee832344c2
jppotess <jppotess@gmail.com>
2016-05-26 22:05:48 -0500

    initial commit

---
+++ wp-content/themes/jp/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * jp Theme Customizer.
+ *
+ * @package jp
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function jp_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'jp_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function jp_customize_preview_js() {
+	wp_enqueue_script( 'jp_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'jp_customize_preview_js' );

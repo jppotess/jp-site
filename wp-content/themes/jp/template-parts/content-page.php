d05dc5f3b4df8b31a1bacdfc47f39add8e68122c
jppotess <jppotess@gmail.com>
2016-05-28 19:42:39 -0500

    remove more class markup - clean2

--- wp-content/themes/jp/template-parts/content-page.php
+++ wp-content/themes/jp/template-parts/content-page.php
@@ -10,22 +10,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div>
 		<?php
 			the_content();
 
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jp' ),
+				'before' => '<div>' . esc_html__( 'Pages:', 'jp' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+	<footer>
 		<?php
 			edit_post_link(
 				sprintf(
@@ -33,7 +33,7 @@
 					esc_html__( 'Edit %s', 'jp' ),
 					the_title( '<span class="screen-reader-text">"', '"</span>', false )
 				),
-				'<span class="edit-link">',
+				'<span>',
 				'</span>'
 			);
 		?>
--- wp-content/themes/jp/template-parts/content-search.php
+++ wp-content/themes/jp/template-parts/content-search.php
@@ -10,21 +10,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	<header>
+		<?php the_title( sprintf( '<h2><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
+		<div">
 			<?php jp_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<div>
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
+	<footer>
 		<?php jp_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->

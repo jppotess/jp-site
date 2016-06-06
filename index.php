599f4a79fdd2b6740726eaab212096f199b68675
jppotess <jppotess@gmail.com>
2016-06-05 19:55:31 -0500

    move repo to just theme

--- index.php
+++ index.php
@@ -1,17 +1,53 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file.
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package jp
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+get_header(); ?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+	<div class="o-container--outer">
+		<div class="o-container--inner">
+			<main class="c-page" role="main">
+
+			<?php
+			if ( have_posts() ) :
+
+				if ( is_home() && ! is_front_page() ) : ?>
+					<header class="c-page__header">
+						<h1 class="c-page__title"><?php single_post_title(); ?></h1>
+					</header>
+
+				<?php
+				endif;
+
+
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'template-parts/content', get_post_format() );
+
+				endwhile;
+
+				the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif; ?>
+
+			</main>
+			<?php get_sidebar(); ?>
+		</div>
+	</div>
+
+<?php
+get_footer();
